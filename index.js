d48fca0a27deab27a2603aaabc3476020c3a95c5
Jordan Olguin <jordanolguin@iMac.local>
2023-07-08 15:56:32 -0600

    added links to work section and added fourth project demo

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello world!");
+const workoutWizardLink = document.querySelector("#workout-wizard-link");
+const roadTripBuddyLink = document.querySelector("#road-trip-buddy-link");
+const weatherDashboardLink = document.querySelector("#weather-dashboard-link");
+const employeeTrackerLink = document.querySelector("#employee-tracker-link");
+
+workoutWizardLink.addEventListener("click", () => {
+  window.location.href =
+    "https://the-workout-wizard-7d5c29c6d6dd.herokuapp.com/";
+});
+
+roadTripBuddyLink.addEventListener("click", () => {
+  window.location.href = "https://dsstad.github.io/travel-weather/";
+});
+
+weatherDashboardLink.addEventListener("click", () => {
+  window.location.href = "https://jordanolguin.github.io/WeatherDashboard/";
+});
+
+employeeTrackerLink.addEventListener("click", () => {
+  window.location.href = "https://github.com/jordanolguin/EmployeeTracker";
+});
