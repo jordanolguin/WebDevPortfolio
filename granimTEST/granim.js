f2ac295ce140f180ac3cad374f872fcc94c73c4b
Jordan Olguin <jordanolguin@iMac.local>
2023-07-11 10:00:22 -0600

    added granim.js test folder

---
+++ granimTEST/granim.js
@@ -0,0 +1,21 @@
+var granimInstance = new Granim({
+  element: "#canvas-image-blending",
+  direction: "top-bottom",
+  isPausedWhenNotInView: true,
+  image: {
+    source:
+      "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    blendingMode: "multiply",
+  },
+  states: {
+    "default-state": {
+      gradients: [
+        ["#29323c", "#485563"],
+        ["#FF6B6B", "#556270"],
+        ["#80d3fe", "#7ea0c4"],
+        ["#f0ab51", "#eceba3"],
+      ],
+      transitionSpeed: 7000,
+    },
+  },
+});
--- index.js
+++ index.js
@@ -40,3 +40,18 @@ resume.addEventListener("click", () => {
     document.body.removeChild(modalContainer);
   });
 });
+
+var granimInstance = new Granim({
+  element: "#canvas-basic",
+  direction: "left-right",
+  isPausedWhenNotInView: true,
+  states: {
+    "default-state": {
+      gradients: [
+        ["#ff9966", "#ff5e62"],
+        ["#00F260", "#0575E6"],
+        ["#e1eec3", "#f05053"],
+      ],
+    },
+  },
+});
